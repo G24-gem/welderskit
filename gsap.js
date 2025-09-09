3f414f91be56ad367e0612efcf8fea447dc102a5
Omoniyi Ayoola <haywhyshotz@gmail.com>
2025-09-09 09:19:44 -0700

    Added animation to the boxes

--- gsap.js
+++ gsap.js
@@ -4,3 +4,34 @@ gsap.from("#text-home",
         duration:3,
     }
 );
+
+gsap.to("#header", {
+    delay: 1,
+    duration: 3,
+    text: {
+        value: "Welcome,Welders!",
+        newClass: "text2",
+        delimiter: "",
+    },
+});
+
+gsap.from("#menu-grid-main",{
+    delay:1,
+    x:-400,
+    duration:3
+});
+gsap.from("#menu-grid-main2",{
+    delay:1,
+    x:400,
+    duration:3
+});
+gsap.from("#menu-grid-main3",{
+    delay:1,
+    x:-400,
+    duration:3
+});
+gsap.from("#menu-grid-main-4",{
+    delay:1,
+    x:400,
+    duration:3
+});
