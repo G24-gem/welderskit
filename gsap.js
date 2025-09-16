b8b896e0a1e951d5e274aac025c7ee68678c34b6
Omoniyi Ayoola <haywhyshotz@gmail.com>
2025-09-16 09:25:04 -0700

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -5,20 +5,31 @@ gsap.from("#text-home",
     }
 );
 
-gsap.to("#header", {
-    delay: 1,
+// Animate multiple texts sequentially on #header
+const texts = [
+  { value: "Maraba, Masu Welding", newClass: "text2" },
+  { value: "Nnọọ, Ndị Welders", newClass: "text3" },
+  { value: "Welcome,Welders!", newClass: "text4" }
+];
+
+let tl = gsap.timeline({ delay: 1 });
+
+texts.forEach(textObj => {
+  tl.to("#header", {
     duration: 3,
     text: {
-        value: "Welcome,Welders!",
-        newClass: "text2",
-        delimiter: "",
-    },
+      value: textObj.value,
+      newClass: textObj.newClass,
+      delimiter: "",
+    }
+  });
 });
 
 gsap.from("#menu-grid-main",{
     delay:1,
     x:-400,
-    duration:3
+    duration:3,
+
 });
 gsap.from("#menu-grid-main2",{
     delay:1,
@@ -34,4 +45,109 @@ gsap.from("#menu-grid-main-4",{
     delay:1,
     x:400,
     duration:3
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Slide in from the LEFT
+gsap.from("#slide", {
+  x: -150,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#slide",
+    start: "top 80%",  // when element is 80% down viewport
+    toggleActions: "play none none none"
+  }
+});
+
+// Slide in from the RIGHT
+gsap.from("#slide2", {
+  x: 150,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#slide2",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+
+// Slide in from the LEFT
+gsap.from("#slide3", {
+  x: -150,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#slide3",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+
+// Slide in from the RIGHT
+gsap.from("#slide4", {
+  x: 150,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#slide4",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+
+// Slide in from the LEFT
+gsap.from("#slide5", {
+  x: -150,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#slide5",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+
+// Slide in from the RIGHT
+gsap.from("#slide7", {
+  x: 150,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#slide4",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+
+//slide in from the left
+gsap.from("#slide8", {
+  x: -150,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#slide5",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+
+// Slide in from the RIGHT
+gsap.from("#slide9", {
+  x: 150,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#slide4",
+    start: "top 80%",
+    toggleActions: "play none none none"
+  }
+});
+
+gsap.to("#animate",{
+    opacity:1,
+    duration:3,
+    y:30
 });
